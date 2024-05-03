edf3f5d6bb1c3f5e1465399b185c1b082d988cad
rnktlhr <rinakitluhur10@gmail.com>
2024-05-03 15:12:07 +0700

    menambahkan while pada boo search

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -57,5 +57,11 @@ void addNode(){
 }
 
 bool search(int rollNo, Node **previous, Node **current){
-
+    *previous = NULL;
+    *current = START;
+    while(*current != NULL && (*current)->noMhs != rollNo){
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return;
 }
